10c45f44463944120bb3310539d99878655bd3b1
shabbir ali <shamilak936@gmail.com>
2024-11-06 15:57:22 +0500

    sm

--- src/components/PhasesSection.vue
+++ src/components/PhasesSection.vue
@@ -2,7 +2,7 @@
     <section class="py-16 bg-gray-100 ">
       <h2 class="text-center text-3xl font-bold mb-8  underline underline-offset-8">3 Phases Pour Élever Votre Présence  Digitale </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-8 justify-evenly justify-items-center ">
-        <!-- Each phase card -->
+        
         <div class="bg-white p-6 rounded-lg shadow-md border-2 border-white rounded-2xl bg-emerald-50">
           <h3 class="text-xl font-semibold mb-4 ">Design</h3>
           <ul class="list-disc ml-4 text-gray-600">
@@ -17,7 +17,7 @@
           </ul>
         </div>
         
-          <!-- Each phase card -->
+          
         <div class="bg-white p-6 rounded-lg shadow-md">
           <h3 class="text-xl font-semibold mb-4">Design</h3>
           <ul class="list-disc ml-4 text-gray-600">
@@ -27,14 +27,14 @@
            
           </ul>
         </div>
-         <!-- Each phase card -->
+       
          <div class="bg-white p-6 rounded-lg shadow-md">
           <h3 class="text-xl font-semibold mb-4">Design</h3>
           <ul class="list-disc ml-4 text-gray-600">
             <li>Maquette sur mesure</li>
             <li>Personnalisation illimitée</li>
             <li>Solution évolutive</li>
-            <!-- Additional list items -->
+           
           </ul>
         </div>
       </div>
