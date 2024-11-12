598390548a1e037095770f91bb6ef864e2b091bd
shabbir ali <qasimfida0054@gmail.com>
2024-11-12 18:21:14 +0500

    line

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="text-center py-16 bg-white font-poppins">
-    <h1 class="text-7xl font-bold mb-4 font-[78px]">Du style, de l’impact, de l’éco.</h1>
-    <p class="text-xl text-center text-gray-700 font-[30px]">
+    <h1 class="text-7xl font-semibold mb-4 font-[78px]">Du style, de l’impact, de l’éco.</h1>
+    <p class="text-xl text-center text-black text-[30px] font-normal mt-[21px]">
       Créez un site séduisant et écoresponsable, alliant esthétique et accessibilité pour tous. 
     </p>
   </section>
--- src/components/WhyEco.vue
+++ src/components/WhyEco.vue
@@ -4,15 +4,15 @@
         <!-- Text Section: Span 4 Columns -->
         <div class="col-span-12 md:col-span-5 items-center lg:ml-[105px] lg:mr-[70px]">
           <div class="w-[69px] h-[5px] bg-gradient-to-b from-[#99E2B4] mb-2"></div>
-          <h2 class="text-4xl font-bold ">
+          <h2 class="text-4xl font-bold text-[#1A202C]">
             Pourquoi un site éco-conçu et accessible ?
           </h2>
-          <p class="mt-4 text-gray-600 text-lg">
+          <p class="mt-4 text-gray-600 text-[25px]">
             Un site éco-responsable et accessible réduit l’impact environnemental du web tout en étant inclusif.
-            Grâce à une conception allégée et des standards d’accessibilité, vous obtenez un site performant,
-            durable et facile d’accès pour tous.
+            Grâce à une conception allégée et des standards d’accessibilité, vous obtenez <a href="#" class="border-b border-black">un site performant,
+              durable et facile d’accès pour tous.</a>
           </p>
-          <a href="#" class="text-blue-600 mt-4 inline-block">En savoir plus →</a>
+          <a href="#" class="text-[#0E0729] font-medium text-[25px] mt-4 inline-block">En savoir plus →</a>
         </div>
         <!-- Image Section: Span 8 Columns -->
         <div class="col-span-12 md:col-span-7 flex justify-center lg:mr-[54px]">
