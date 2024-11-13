750a6c1b218c469cd8bf47a1958958bf66566423
shabbir ali <qasimfida0054@gmail.com>
2024-11-14 01:19:19 +0500

    card componet

---
+++ src/assets/svgs/AccessibilityIcon.vue
@@ -0,0 +1,38 @@
+<!-- src/components/icons/AccessibilityIcon.vue -->
+<template>
+    <svg
+      class="w-[40px] h-[40px] md:w-[50px] md:h-[50px]"
+      width="51"
+      height="51"
+      viewBox="0 0 51 51"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M25.4367 18.2144C18.6025 18.2144 12.1147 23.1306 12.9307 29.916C12.9941 30.4431 13.0698 30.9962 13.1577 31.5845C13.4193 33.3352 14.9459 34.6072 16.7161 34.6072H18.7395L20.2158 43.7723C20.6331 46.3631 22.8692 48.2679 25.4935 48.2679C28.1177 48.2679 30.3538 46.3631 30.7712 43.7723L32.2474 34.6072H34.2969C36.0555 34.6072 37.5776 33.3524 37.8459 31.6144C37.9317 31.0591 38.0054 30.5347 38.067 30.0318C38.904 23.1981 32.3215 18.2144 25.4367 18.2144Z"
+        fill="#99E2B4"
+        stroke="#020202"
+        stroke-width="2.475"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M25.5001 17.3036C29.5239 17.3036 32.7858 14.0417 32.7858 10.0179C32.7858 5.9941 29.5239 2.73218 25.5001 2.73218C21.4763 2.73218 18.2144 5.9941 18.2144 10.0179C18.2144 14.0417 21.4763 17.3036 25.5001 17.3036Z"
+        fill="#99E2B4"
+        stroke="#020202"
+        stroke-width="2.475"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AccessibilityIcon',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any scoped styles if necessary */
+  </style>
+  
---
+++ src/assets/svgs/DesignIcon.vue
@@ -0,0 +1,19 @@
+<!-- src/components/icons/DesignIcon.vue -->
+<template>
+    <svg width="39" height="39" viewBox="0 0 39 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M21.125 34.125H34.125" stroke="white" stroke-opacity="0.99" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M32.6058 12.0156L11.1706 33.5364C10.7953 33.9132 10.2853 34.125 9.75353 34.125L6.50623 34.125C5.60532 34.125 4.875 33.3882 4.875 32.4873V29.219C4.875 28.69 5.08462 28.1825 5.45797 27.8076L26.8965 6.28356C31.7902 2.18922 36.6839 7.92126 32.6058 12.0156Z" stroke="white" stroke-opacity="0.99" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M24.8782 8.62842L30.4321 14.1822" stroke="white" stroke-opacity="0.99" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DesignIcon',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any scoped styles if necessary */
+  </style>
+  
