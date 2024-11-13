263e53141562db30d5d3ec9f2b42de789973446a
shabbir ali <qasimfida0054@gmail.com>
2024-11-14 01:24:38 +0500

    why eco

--- src/components/WhyEco.vue
+++ src/components/WhyEco.vue
@@ -1,31 +1,32 @@
 <template>
-    <section class="py-[109px] font-poppins">
-      <div class="grid grid-cols-12 gap-6   flex justify-center">
-        <div class="col-span-12 lg:col-span-5 px-2 lg:px-0 items-center lg:ml-[80px] lg:mr-[70px]">
-          <div class="w-full md:w-[69px] h-[5px] bg-gradient-to-b from-[#99E2B4] mb-[20px]"></div>
-          <h2 class="text-[18px] text-center md:text-left md:text-[33px] font-bold font-inter">
-            Pourquoi un site éco-conçu et accessible ?
-          </h2>
-          <p class="text-[19px] font-inter text-center md:text-left font-normal pb-[51px]">
-            Un site éco-responsable et accessible réduit l’impact environnemental du web tout en étant inclusif.
-            Grâce à une conception allégée et des standards d’accessibilité, vous obtenez <span class="underline underline-offset-[5px]"> un site performant,
+  <section class="py-[109px] font-poppins md:max-w-[1700px] mx-10 md:mx-auto">
+    <div class="grid grid-cols-12 gap-6   flex justify-center ">
+      <div class="col-span-12 lg:col-span-5 px-2 lg:px-0 items-center ">
+        <div class="w-full md:w-[69px] h-[5px] bg-gradient-to-b from-[#99E2B4] mb-[20px]"></div>
+        <h2 class="text-[18px] text-center md:text-left md:text-[33px] font-bold font-inter">
+          Pourquoi un site éco-conçu et accessible ?
+        </h2>
+        <p class="text-[19px] font-inter text-center md:text-left font-normal pb-[51px]">
+          Un site éco-responsable et accessible réduit l’impact environnemental du web tout en étant inclusif.
+          Grâce à une conception allégée et des standards d’accessibilité, vous obtenez <span
+            class="underline underline-offset-[5px]"> un site performant,
             durable et facile d’accès pour tous.</span>
-          </p>
-          <div class="flex justify-center md:justify-start">
-  <a href="#" class="text-[#0E0729] w-[204px] h-[25px] font-medium text-[25px] mb-[46px] inline-block">
-    En savoir plus →
-  </a>
-</div>
-          <!-- <a href="#" class="text-[#0E0729]  w-[204px] h-[25px] font-medium text-[25px] inline">En savoir plus →</a> -->
+        </p>
+        <div class="flex justify-center md:justify-start">
+          <a href="#" class="text-[#0E0729] w-[204px] h-[25px] font-medium text-[25px] mb-[46px] inline-block">
+            En savoir plus →
+          </a>
         </div>
-        <div class="col-span-12 hidden md:block lg:col-span-7 flex justify-center lg:mr-[54px]">
-          <img src="../assets/img/img-1.png" alt="Image" class="rounded-xl w-full md:w-[718px] h-auto md:h-[576px] object-cover " />
-        </div>
+        <!-- <a href="#" class="text-[#0E0729]  w-[204px] h-[25px] font-medium text-[25px] inline">En savoir plus →</a> -->
       </div>
-    </section>
-  </template>
-  
-  <script>
-  export default {};
-  </script>
-  
+      <div class="col-span-12 hidden md:block lg:col-span-7 flex justify-center ">
+        <img src="../assets/img/img-1.png" alt="Image"
+          class="rounded-xl w-full  h-auto md:h-[576px] object-cover " />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
